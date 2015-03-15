f5405a672299b18e0b6b91834b9bf61116942ef5
Dwayne Macgowan <dwaynemac@gmail.com>
2015-03-15 17:33:25 -0300

    add padma_id to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315192404) do
+ActiveRecord::Schema.define(version: 20150315202337) do
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20150315192404) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "padma_id"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
