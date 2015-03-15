4be99d03afb9479ff57589c6c949f2347b9d55a5
Luis Perichon <luisperichon@gmail.com>
2015-03-15 17:04:38 -0300

    navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315172227) do
+ActiveRecord::Schema.define(version: 20150315172831) do
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
